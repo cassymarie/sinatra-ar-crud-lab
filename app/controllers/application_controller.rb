53290c03c44811b3abb1081cf0c6ed259746d43c
cassymarie <cassy2205@gmail.com>
2020-12-02 13:01:02 -0700

    commit tree 948dd4ba8f1fa8a3ba314224a4da2aec23ca08cd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  #read articles
+  get '/articles' do
+    @articles = Article.all
+    erb :index
   end
+
+  #create article
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to "/articles/#{@article.id}"
+  end
+
+  #new form
+  get '/articles/new' do
+    erb :new
+  end
+
+  #read article by id
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+     
+  #* Get edit Article form
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  #* Update Article
+  patch '/articles/:id' do
+        @article = Article.find(params[:id])
+        @article.update(params[:article])
+        redirect to "/articles/#{@article.id}"
+  end
+
+  #* delete Article
+  delete '/articles/:id' do
+      Article.destroy(params[:id])
+      redirect to "/articles"
+  end
+
 end
